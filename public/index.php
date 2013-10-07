63772e5c3b1de9444f091d222d92b1ed267c3c64
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-07 13:44:29 +0200

    add some bootstrapping

--- public/index.php
+++ public/index.php
@@ -5,6 +5,16 @@
  */
 chdir(dirname(__DIR__));
 
+// Define application environment ZF1 style
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?: 'production'));
+
+// Report everything
+error_reporting(E_ALL | E_STRICT);
+
+// Disable error displaying in production
+ini_set("display_errors", APPLICATION_ENV != 'production');
+
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
